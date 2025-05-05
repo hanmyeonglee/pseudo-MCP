fe5116ba63f202d6f6da93e895133fc3649ec9cb
Hann <ejongwon7@gmail.com>
2025-05-05 20:00:37 +0900

    add some new tools

--- mcp_server/tools.py
+++ mcp_server/tools.py
@@ -1,4 +1,4 @@
-import time, datetime
+import time, datetime, random
 
 def healthcheck():
     """Check the connection health between client and server. Always return true.
@@ -34,3 +34,28 @@ def repeat_after_me(sentence: str):
     """
     return sentence
 
+def random_number_generator(a: int, b: int, n: int = 1):
+    """Generate array of n random numbers, n is 1 at default state. The number is integer and in the range of a and less than b. If a >= b, than return the empty array.
+
+    Args:
+        a (int)
+        b (int)
+        n (int, optional)
+
+    Returns:
+        array
+    """
+    if a >= b: return []
+    return [random.randrange(a, b) for _ in range(n)]
+
+def give_word_randomly_from_given_word_array(words: list[str]):
+    """Give oen random word from given words array. If array is empty, than return value is null.
+
+    Args:
+        words (array)
+
+    Returns:
+        string | null
+    """
+    if len(words) == 0: return None
+    return random.choice(words)
